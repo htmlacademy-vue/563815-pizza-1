44be015e6ebc4eabac441937e97ab599ab5d76cc
Yuri Mikhin <urasergeevich@mail.ru>
2022-05-02 20:56:14 +0300

    Refactor styles in PageHeader.

--- src/frontend/src/common/components/PageHeader.vue
+++ src/frontend/src/common/components/PageHeader.vue
@@ -17,7 +17,7 @@ export default {
       </a>
     </div>
     <div class="page-header__cart">
-      <a href="#">0 ₽</a>
+      <a class="page-header__cart-link" href="#">0 ₽</a>
     </div>
     <div class="page-header__user">
       <a href="#" class="page-header__login"><span>Войти</span></a>
@@ -29,11 +29,8 @@ export default {
 .page-header {
   position: relative;
   z-index: 2;
-
   display: flex;
-
   padding: 0 2.12%;
-
   background-color: $green-500;
   box-shadow: $shadow-light;
 }
@@ -46,94 +43,63 @@ export default {
 .page-header__cart {
   margin-right: 10px;
   margin-left: auto;
-
-  a {
-    @include b-s16-h19;
-
-    display: block;
-
-    padding: 21px 15px 21px 58px;
-
-    transition: 0.3s;
-
-    color: $white;
-    background-color: $green-500;
-    background-image: url("~@/assets/img/cart.svg");
-    background-repeat: no-repeat;
-    background-position: 20px center;
-    background-size: 29px 27px;
-
-    &:hover:not(:active) {
-      background-color: $green-400;
-    }
-
-    &:active {
-      background-color: $green-600;
-    }
-
-    &:focus {
-      opacity: 0.5;
-    }
-  }
 }
 
-.page-header__user {
-  a {
-    display: block;
+.page-header__cart-link {
+  @include b-s16-h19;
 
-    padding: 14px 20px;
+  display: block;
+  padding: 21px 15px 21px 58px;
+  transition: 0.3s;
+  color: $white;
+  background-color: $green-500;
+  background-image: url("~@/assets/img/cart.svg");
+  background-repeat: no-repeat;
+  background-position: 20px center;
+  background-size: 29px 27px;
 
-    transition: 0.3s;
-
-    background-color: $green-500;
-
-    &:hover:not(:active) {
-      background-color: $green-400;
-    }
-
-    &:active {
-      background-color: $green-600;
-    }
-
-    &:focus {
-      opacity: 0.5;
-    }
+  &:hover:not(:active) {
+    background-color: $green-400;
   }
 
-  img {
-    display: inline-block;
-
-    width: 32px;
-    height: 32px;
-    margin-right: 8px;
-
-    vertical-align: middle;
-
-    border-radius: 50%;
+  &:active {
+    background-color: $green-600;
   }
 
-  span {
-    @include r-s14-h16;
-
-    display: inline-block;
-
-    vertical-align: middle;
-
-    color: $white;
+  &:focus {
+    opacity: 0.5;
   }
 }
 
 .page-header__login {
+  @include r-s14-h16;
+
+  display: inline-block;
+  padding: 14px 20px;
+  transition: 0.3s;
+  background-color: $green-500;
+  vertical-align: middle;
+  color: $white;
+
+  &:hover:not(:active) {
+    background-color: $green-400;
+  }
+
+  &:active {
+    background-color: $green-600;
+  }
+
+  &:focus {
+    opacity: 0.5;
+  }
+
   &::after {
     display: inline-block;
-
     width: 32px;
     height: 32px;
     margin-left: 8px;
-
     content: "";
     vertical-align: middle;
-
     background: url(~@/assets/img/login.svg) no-repeat center;
     background-size: auto 50%;
   }
